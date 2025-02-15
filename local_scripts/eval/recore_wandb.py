dcf263b55a55f2a8fac4df399b78650d00c7f926
KaneLin <linkangheng@megvii.com>
2025-02-15 15:24:51 +0800

    update recore_wandb.py

--- local_scripts/eval/recore_wandb.py
+++ local_scripts/eval/recore_wandb.py
@@ -16,6 +16,9 @@ runs = [
     ("qwen2-vl-2b_vllm_perpo_roll16", 80.07, 79.56),
     ("qwen2-vl-2b_vllm_perpo_roll32", 80.39, 79.97),
     ("qwen2-vl-2b_vllm_perpo_roll64", 80.43, 80.06),
+    ("qwen2-vl-2b_vllm_perpo_roll126", 80.62, 80.24),
+    ("qwen2-vl-2b_vllm_perpo_roll252", 80.56, 79.98),
+    ("qwen2-vl-2b_vllm_perpo_roll329", 80.76, 80.14),
 ]
 
 for run_name, refcocog_val, refcocog_test in runs:
