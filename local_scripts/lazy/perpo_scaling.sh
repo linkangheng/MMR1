f6d83b39c6973a5873da74110963ea354640ba1a
KaneLin <linkangheng@megvii.com>
2025-02-23 15:39:36 +0800

    delete irrelevant files

--- local_scripts/lazy/perpo_scaling.sh
+++
@@ -1,7 +0,0 @@
-scales=(1 5 10 20)
-base_size=3000
-
-for scale in ${scales[@]}; do
-    train_sample_size=$((${base_size} * ${scale}))
-    bash /data/ICCV2025/PaR/MMR1/local_scripts/train/grounding/train_qwen2_vl_perpo_scaling.sh ${train_sample_size}
-done
--- local_scripts/lazy/qwen2vl_scaling.sh
+++
@@ -1,3 +0,0 @@
-for i in 2 4 8 16 32 64 128; do
-    bash local_scripts/train/train_qwen2_vl_perpo.sh $i
-done
